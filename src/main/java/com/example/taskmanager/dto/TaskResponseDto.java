597c0a1834be5131cc9d57652606905c7d90457f
Ddiy0ng <bueong0627@gmail.com>
2025-05-10 18:44:26 +0900

    Modify: 날짜 포맷팅 수정
    응답에서 날짜 포맷 YYYY-MM-DD로 고정
    
    db에는 년, 월, 일에 시간도 저장

--- src/main/java/com/example/taskmanager/dto/TaskResponseDto.java
+++ src/main/java/com/example/taskmanager/dto/TaskResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.taskmanager.dto;
 
 import com.example.taskmanager.entity.Task;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.sql.Timestamp;
@@ -12,7 +13,9 @@ public class TaskResponseDto {
     private Long taskId;
     private String name;
     private String tasks;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Timestamp postDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Timestamp updateDate;
 
     //dto 반환
